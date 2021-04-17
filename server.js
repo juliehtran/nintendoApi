ca3497bed8c03f0621ab94a0aa484b5df8982782
Julie Tran <tranjhdesign@gmail.com>
2021-04-16 22:09:21 -0400

    changed port 8000 to 8080
    * and allow env var for dynamic heroku port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const PORT = 8000
+const PORT = process.env.PORT || 8080
 
 const data = require('./info.js')
 
